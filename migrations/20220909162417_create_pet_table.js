9cf75f99aab39f53959f394fd7bb07a95f7e2f7d
Matthew Greenup <matthewgreenup@Green-Machine.home>
2022-09-09 09:44:22 -0700

    second commit

---
+++ migrations/20220909162417_create_pet_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('pet', table => {
+        table.increments();
+        table.string('name', 250);
+        table.foreign()
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
